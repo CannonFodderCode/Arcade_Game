5fa40d98e5310780b9d5fba8014b5e42aa7fbe49
George <168304265+CannonFodderCode@users.noreply.github.com>
2024-06-11 00:06:41 +0100

    Added bones for gamestates

--- Main.py
+++ Main.py
@@ -15,10 +15,7 @@ Later to include:
 '''
 #TO-DO:
 '''
-Enemy tries to shoot through walls...?!
-
 Fix enemy collision with walls
-Allow MapMaker to place enemies and ground instead of just empty blocks
 '''
 import pygame, sys, Launcher
 from pygame.locals import *
@@ -51,40 +48,46 @@ screensize = (scr_wi, scr_hi)
 
 enemies = pygame.sprite.Group()
 projectiles = pygame.sprite.Group()
-dave = enemy((670, 1120), screensize)
-print(f"main enemy created at {dave.rect.center}")
-enemies.add(dave)
+#dave = enemy((670, 1120), screensize)
+#print(f"main enemy created at {dave.rect.center}")
+#enemies.add(dave)
+
+gamestate = 1    # 0 = Menu, 1 = Playing, 2 = Pause, 
 
 if __name__ == "__main__":
     SetUpMap(area_map, trim_data(worlddata), (150,120,40), (100,40,20), 80, screensize, enemy, enemies)   # sets up the surface "area_map" with a world image from "worlddata" usign the specified colours
     while True:
-        pressed_keys = pygame.key.get_pressed()
-        disp.fill((150,120,40))
+        if gamestate == 0:  # Main Menu
+            pass
+        elif gamestate == 1:  # Main game
+            pressed_keys = pygame.key.get_pressed()
+            disp.fill((150,120,40))
 
-        map_DX, map_DY = bob.move(blocks, scr_wi, scr_hi, projectiles)  # moves the player, and returns delta [X, Y]
+            map_DX, map_DY = bob.move(blocks, scr_wi, scr_hi, projectiles)  # moves the player, and returns delta [X, Y]
 
-        X_offset = int(((cursor.rect.centerx)-scr_wi//2) /5)  # denominator will become weapon-specific
-        Y_offset = int(((cursor.rect.centery)-scr_hi//2) /4)  # close range weapons = low denom. vice versa
-        X_drift += (X_offset - X_drift)/40  # gradually moves the camera offset towards the cursor
-        Y_drift += (Y_offset - Y_drift)/40  # reduce the denominator to increase speed
+            X_offset = int(((cursor.rect.centerx)-scr_wi//2) /5)  # denominator will become weapon-specific
+            Y_offset = int(((cursor.rect.centery)-scr_hi//2) /4)  # close range weapons = low denom. vice versa
+            X_drift += (X_offset - X_drift)/40  # gradually moves the camera offset towards the cursor
+            Y_drift += (Y_offset - Y_drift)/40  # reduce the denominator to increase speed
 
-        map_position = (map_position[0] + map_DX, map_position[1] + map_DY)  # updates the map position by player movement to keep them in sync
-        drifted_map_pos = (map_position[0] - X_drift, map_position[1] - Y_drift)
+            map_position = (map_position[0] + map_DX, map_position[1] + map_DY)  # updates the map position by player movement to keep them in sync
+            drifted_map_pos = (map_position[0] - X_drift, map_position[1] - Y_drift)
 
-        disp.blit(area_map, drifted_map_pos)
-        cursor.draw(disp)
+            disp.blit(area_map, drifted_map_pos)
+            cursor.draw(disp)
 
-        for item in enemies:   # handling of enemy animations
-            item.move(blocks)
-            item.attack(bob, blocks, projectiles)
-            item.draw(disp, drifted_map_pos, blocks)
+            for item in enemies:   # handling of enemy animations
+                item.move(blocks)
+                item.attack(bob, blocks, projectiles)
+                item.draw(disp, drifted_map_pos, blocks)
 
-        bob.draw(disp, (scr_wi//2 - X_drift, scr_hi//2 - Y_drift))  # draws player at the center of the screen, adjusted by cursor location
+            bob.draw(disp, (scr_wi//2 - X_drift, scr_hi//2 - Y_drift))  # draws player at the center of the screen, adjusted by cursor location
 
-        for item in projectiles:   # handling of projectile animations
-            item.update(blocks)
-            item.draw(disp, drifted_map_pos)
-
+            for item in projectiles:   # handling of projectile animations
+                item.update(blocks)
+                item.draw(disp, drifted_map_pos)
+        elif gamestate == 2:  # Pause
+            pass
         pygame.display.flip()
 
         for event in pygame.event.get():
